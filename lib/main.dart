e0ff8f34ce2f7d356234f08623ab48c01fd62d2d
Thao <thaolp2611@gmail.com>
2021-12-06 07:52:18 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Todo task',
+      debugShowCheckedModeBanner: false,
+      home: Text('Todo tasks'),
+    ),
+  );
+}
+
+
